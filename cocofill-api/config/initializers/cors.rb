085e2d8a60723bbc31267d7d65502b7397c283d8
momo-41 <minimizu41@gmail.com>
2024-11-23 22:51:33 +0900

    cors.rbを修正

--- cocofill-api/config/initializers/cors.rb
+++ cocofill-api/config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://cocofill-two.vercel.app", "http://localhost:3001"
+    origins "https://cocofill-next.vercel.app", "http://localhost:3001"
     # ポート番号の違うNext.js側のアクセス(3000)を許可する
 
     resource "*",
